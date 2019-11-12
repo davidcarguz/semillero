971996583696a3d377e14f76650229a6d2e100e1
Daniel Grajales <daniel.grajales@tcs.com>
2019-11-12 12:29:58 -0500

    correción1

--- src/evaluacionUno/NuevaClase.java
+++ src/evaluacionUno/NuevaClase.java
@@ -3,15 +3,9 @@ package evaluacionUno;
 public class NuevaClase {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-		System.out.println("Hola!");
-=======
-		System.out.println("jhindy :))))!");
->>>>>>> 0ab6ca24bedc362c2581d4899e8aef9b7e9704ea
-=======
-		System.out.println("Cambiando mensaje :D");
->>>>>>> 6e8f9f63e0d33970c61d4382dca9519637aa0195
+
+		System.out.println("fue Jhindy");
+
 
 	}
 
